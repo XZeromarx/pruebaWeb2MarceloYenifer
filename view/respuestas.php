d39adc91ef02c0bce8692cf8c23c1d4037e8e1d4
yeniferosorio <sofiaa_yenifer@hotmail.com>
2018-05-23 20:08:52 -0400

    se mejora las tablas de respuestas

---
+++ view/respuestas.php
@@ -0,0 +1,98 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../model/bulma-0.7.1/css/bulma.min.css">
+        <title>Resultados</title>
+    </head>
+    <body>
+        <div class="box is-centered">
+
+            <div class="columns">
+                <div class="column"></div>
+
+                <div class="column">
+                    <table class=" table is-bordered is-striped is-narrow is-hoverable is-fullwidth is-centered">
+                        <h1><b>RESPUESTAS</b></h1>
+                        <br></br>
+
+                        <h1>PREGUNTA 1</h1>
+
+                        <tr>
+                            <td>Encuesta</td>
+
+                            <td>respuesta</td>
+
+                        </tr> 
+
+                        <tr>
+
+                            <td>Encuesta </td>
+
+                            <td>respuesta</td>
+
+                        </tr>
+
+
+                    </table>
+                    
+                    <table class=" table is-bordered is-striped is-narrow is-hoverable is-fullwidth is-centered">
+                        <h1>PREGUNTA 2</h1>
+                     
+
+                        <tr>
+                            <td>Encuesta </td>
+
+                            <td>respuesta</td>
+
+                        </tr> 
+
+                        <tr>
+
+                            <td>Encuesta </td>
+
+                            <td>respuesta</td>
+
+                        </tr>
+
+
+                    </table>
+                    
+                    <table class=" table is-bordered is-striped is-narrow is-hoverable is-fullwidth is-centered">
+                    <h1>PREGUNTA 3</h1>
+
+                        <tr>
+                            <td>Encuesta</td>
+
+                            <td>respuesta</td>
+
+                        </tr> 
+
+                        <tr>
+
+                            <td>Encuesta </td>
+
+                            <td>respuesta</td>
+
+                        </tr>
+
+
+                    </table>
+
+
+
+                    <div class="column"></div>
+
+                </div>
+
+
+                <div class="column"></div>
+
+            </div>
+
+
+
+
+    </body>
+</html>
